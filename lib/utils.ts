d9322ab8db9e25001262b3b3861b9e2476152608
Bo <robertsworth@gmail.com>
2025-02-01 20:29:25 -0500

    Fix UI component exports and configuration

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: (string | undefined)[]) {
-  return inputs.filter(Boolean).join(" ")
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
